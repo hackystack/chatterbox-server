d72013b121b657bf55305236e1330b1882f93005
hackystack <dorpalenbarry@gmail.com>
2014-06-24 19:17:51 -0700

    changed port to 8081

--- server/basic-server.js
+++ server/basic-server.js
@@ -6,13 +6,13 @@ var handler = require("./request-handler.js").handleRequest;
  * standard port for HTTP servers is port 80, but that port is
  * normally already claimed by another server and/or not accessible
  * so we'll use a higher port number that is not likely to be taken: */
-var port = 80;
+var port = 8081;
 // var port = 3000;
 
 /* For now, since you're running this server on your local machine,
  * we'll have it listen on the IP address 127.0.0.1, which is a
  * special address that always refers to localhost. */
-// var ip = "127.0.0.1";
+var ip = "127.0.0.1";
 
 
 
@@ -22,7 +22,7 @@ will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in t
 Lastly, we tell the server we made to listen on the given port and IP. */
 var server = http.createServer(handler);
 console.log("Listening on http://" + ip + ":" + port);
-server.listen(port);
+server.listen(port,ip);
 
 /* To start this server, run:
      node basic-server.js
